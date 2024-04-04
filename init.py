568cd1afd2a13a66e7e0036795ab85a6c8c0af83
BredanKM <bredankm@gmail.com>
2024-04-04 11:50:34 -0500

    initialized supabase database connection

--- init.py
+++ init.py
@@ -1,2 +1,13 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+
 import os
 from supabase import create_client, Client
+
+url = os.getenv("SUPABASE_URL")
+key = os.getenv("SUPABASE_KEY")
+
+supabase: Client = create_client(url, key)
+
+print(supabase)
